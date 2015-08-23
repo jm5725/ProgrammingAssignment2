d84be8050f68c45484270dd3416ac4c4264c60e8
jm5725 <jm5725@att.com>
2015-08-23 18:03:49 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,31 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+     mcm <- NULL
+     set<- function (cche){
+     x<<-cche
+     mcm <<- NULL
+     }
+     gtcche <- function() x
+     setmtx <- function(solve) mcm<<-solve
+     getmtx <- function()mcm
+     list(set=set,gtcche=gtcche,setmtx=setmtx,getmtx=getmtx)
+     
 }
 
 
 ## Write a short comment describing this function
 
+
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  mcm<-x$getmtx()
+  if (!is.null(m)){
+       message("getmtx returning cached data")
+       return(mcm)
+  }
+   matrix<-x$gtcche()
+   mcm<-solve(matrix, ...)
+   x$setmtx(mcm)
+   mcm
 }
